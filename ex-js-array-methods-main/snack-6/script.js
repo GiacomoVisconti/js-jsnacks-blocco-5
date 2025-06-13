c519a6957cff5c54188d61c167554ae6d37290aa
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-13 14:31:52 +0200

    snack 6 completion

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,47 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+//Con il ciclo FOR EACH
+const min_15 = []
+const max_15 = []
+
+zucchine.forEach((element)=>{
+  if (element.length > 15) {
+    min_15.push(element)
+    
+  }else max_15.push(element)
+})
+
+console.log(min_15);
+console.log(max_15);
+
+
+//Con il ciclo FILTER
+const zucc_max_15 = zucchine.filter((element) => {
+  if (element.length < 15) {
+    return element
+  } 
+})
+const zucc_min_15 = zucchine.filter((element) => {
+  if (element.length > 15) {
+    return element
+  } 
+})
+
+console.log(zucc_min_15);
+console.log(zucc_max_15);
+
+//Con il metodo MAP 
+const corte_15 = zucchine.map((element)=> {
+  if (element.length < 15) {
+    return element
+  } 
+}) 
+const lunghe_15 = zucchine.map((element)=> {
+  if (element.length > 15) {
+    return elements
+  } 
+}) 
+console.log(corte_15);
+console.log(lunghe_15);
