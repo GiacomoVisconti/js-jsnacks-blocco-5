b3cbefc04e308d42d4015bbf69f396c7ecd4d854
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-13 14:59:37 +0200

    snack 8 completion

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -20,4 +20,40 @@ const students = [
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
+const to_search = 'Marco Lanci'
+
+//Con il metodo FIND
+const result = students.find((element) =>{
+  const classe = element.class
+
+  if (element.name === to_search) {
+    return classe
+  }
+})
+console.log(result);
+
+//con il ciclo FOR EACH
+
+let risultato = null
+
+students.forEach((element) => {
+  if (element.name === to_search) {
+    risultato = element.class
+  }
+})
+console.log(risultato);
+
+
+//Con Il metodo FIRLTER 
+const result1 = students.filter((element) =>{
+  const classe = element.class
+
+  if (element.name === to_search) {
+    return classe
+  }
+})
+console.log(result1);
+
+
+
 // Risultato: '3C'
