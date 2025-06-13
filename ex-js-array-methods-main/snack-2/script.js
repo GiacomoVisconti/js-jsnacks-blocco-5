8c624774f6562786cf09de6b430ab1f8185af6f2
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-13 13:49:32 +0200

    snack 2 completion

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -5,4 +5,32 @@ const people = [
 ];
 
 // Stampa in console tutti i nomi
+
+//Con il ciclo FOR EACH
+
+const nomi = []
+people.forEach((element) => {
+  //oppure: console.log(element.name);
+  nomi.push(element.name)
+})
+console.log(nomi);
+
+//Con il ciclo FOR
+const names = [];
+for (let i = 0; i < people.length; i++) {
+  const element = people[i];
+  
+  // oppure: console.log(element.name);
+  names.push(element.name)
+  
+}
+console.log(names);
+
+
+//Con il metodo MAP
+const first_names = people.map((element)=>{
+  return element.name
+})
+console.log(first_names);
+
 // Risultato: 'Paolo', 'Giulia', 'Marco'
