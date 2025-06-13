d5462918c58ab847db6b247ae3172a41bc6b4e72
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-13 13:38:11 +0200

    scaffolding and snack 1 completion

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,22 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+
+//Col ciclo for EACH
+names.forEach((element) => {
+    console.log(element);
+    
+})
+
+//Col ciclo FOR
+
+for (let i = 0; i < names.length; i++) {
+    const element = names[i];
+    
+    console.log(element);
+    
+}
+
+//Col ciclo 
+
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
